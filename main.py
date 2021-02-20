56a548c7232420254b7eed105f35f66a500617d4
hamna-coder <75372452+hamna-coder@users.noreply.github.com>
2021-02-20 12:28:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -31,35 +31,7 @@ while True:
         KeyAlphabet(x_axis, y_axis, w +10, h)
 
 
-    if delay > 170:
-            if x_axis == 550 and y_axis == 85:
-                x_axis = 615
-            if x_axis == 550 and y_axis == 160:
-                x_axis = 615
-            if x_axis == 30 and y_axis == 235:
-                x_axis = 250
-            if x_axis == 315 and y_axis == 235:
-                x_axis = 445
-            if x_axis == 510 and y_axis == 235:
-                x_axis = 520
-            if x_axis == 585 and y_axis == 235:
-                x_axis = 595
-            if x_axis == 660 and y_axis == 235:
-                x_axis = 680
-            if x_axis == 745 and y_axis == 235:
-                x_axis = -35
-                y_axis = 10
+  
+        
 
-            x_axis += 65
-            if x_axis > 850:
-                x_axis = 30
-                y_axis += 75
-            if y_axis == 300:
-                x_axis=30
-                y_axis =0
-            delay =0
-    else:
-            delay += 1
-
-    if cv2.waitKey(1)==27:
-        break
+   
